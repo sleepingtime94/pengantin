e9c4ffa84b114a62ce99fb306fa3a7679bf29ff4
Madhana <sleepingtime94@gmail.com>
2025-11-20 12:00:22 +0800

    au ah

--- views/dashboard/index.blade.php
+++ views/dashboard/index.blade.php
@@ -3,7 +3,13 @@
 @section('title', 'DASHBOARD - PENGANTIN')
 
 @section('content')
-
-<h1>Maintenance</h1>
-
+@if (!empty($users) && isset($users['user_step']))
+    @if ($users['user_step'] == 1)
+        @include('dashboard.formulir')
+    @elseif ($users['user_step'] == 2)
+        @include('dashboard.upload')
+    @elseif ($users['user_step'] == 3)
+        @include('dashboard.timeline')
+    @endif
+@endif
 @endsection
