00ed0842cb32e423afbcb550fe1e13f3527ba6a0
Madhana <sleepingtime94@gmail.com>
2025-11-20 11:56:11 +0800

    tes

--- views/dashboard/index.blade.php
+++ views/dashboard/index.blade.php
@@ -4,14 +4,6 @@
 
 @section('content')
 
-@if (!empty($users) && isset($users['user_step']))
-@if ($users['user_step'] == 1)
-@include('dashboard.formulir')
-@elseif ($users['user_step'] == 2)
-@include('dashboard.upload')
-@elseif ($users['user_step'] == 3)
-@include('dashboard.timeline')
-@endif
-@endif
+<h1>Maintenance</h1>
 
 @endsection
